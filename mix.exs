8093be6f2517834974abc130ffc193384f512780
Roman Chvanikov <chvanikoff@gmail.com>
2017-06-29 17:44:57 +0900

    Updated version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ER.Mixfile do
 
   def project do
     [app: :elixir_run,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
